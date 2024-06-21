15743392564e59cb1104700a47d034d72061bbf3
aditya <aditya.pandey.241103@gmail.com>
2024-06-21 16:43:26 +0530

    osaic landing page complete

---
+++ src/app/osaic/page.jsx
@@ -0,0 +1,14 @@
+import OsaicHeader from '@/components/osaic/Header'
+import Landing from '@/components/osaic/Landing'
+import React from 'react'
+
+const OsaicPage = () => {
+    return (
+        <div>
+            <OsaicHeader />
+            <Landing />
+        </div>
+    )
+}
+
+export default OsaicPage
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,9 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      paddingClamp: {
+        'custom': 'padding-left: clamp(20px, calc(12.95774647887324px + 1.8779342723004695vw), 40px); padding-right: clamp(20px, calc(12.95774647887324px + 1.8779342723004695vw), 40px);',
+      },
     },
   },
   plugins: [],
