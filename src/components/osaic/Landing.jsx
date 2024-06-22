dd90129a38f5451fba7a23f40a61e223118d7cd4
aditya <aditya.pandey.241103@gmail.com>
2024-06-22 14:11:46 +0530

    osaic landing complete

--- src/components/osaic/Landing.jsx
+++ src/components/osaic/Landing.jsx
@@ -1,7 +1,12 @@
+import PicturesGrid from "./PicturesGrid";
+
 const Landing = () => {
   return (
     <div className="relative mx-auto mb-11 max-w-[1500px]">
-      <div className="gap-xs prose-animate-in relative z-10 order-2 grid grid-cols-12 px-[20px] lg:px-24">
+      <div className="relative left-0 top-0 order-1 aspect-[1] h-1/2 w-full md:absolute md:m-auto md:h-full">
+        <PicturesGrid />
+      </div>
+      <div className="relative z-10 order-2 grid grid-cols-12 px-[20px] lg:px-24">
         <div className="col-span-12 pb-11 pt-8 md:col-span-4 md:pb-32 md:pt-52">
           <div className="style-label pb-8 text-[#f8614b]">MEET OSAIC</div>
           <h1 className="pb-2 text-[42px] leading-[50.4px] text-[#15535e]">
@@ -46,114 +51,6 @@ const Landing = () => {
               </svg>
             </span>
           </button>
-        </div>
-      </div>
-
-      <div className="relative left-0 top-0 order-1 aspect-[1] h-1/2 w-full md:absolute md:m-auto md:h-full">
-        <div className="absolute left-0 top-0 h-full w-full md:left-[45%] md:w-1/2">
-          <div className="animate-slide-up-in absolute left-0 top-0 h-[48%] w-[15%] [animation-delay:0.2s] md:w-[45%]">
-            <div className="absolute inset-0 h-full w-full overflow-hidden rounded-r-[13px] md:rounded-b-[30px] md:rounded-t-[0]">
-              <img
-                alt=""
-                fetchPriority="high"
-                decoding="async"
-                data-nimg="fill"
-                className="absolute inset-0 size-full object-cover object-center text-transparent"
-                style={{ objectPosition: "62.1076% 53.5283%" }}
-                sizes="25vw"
-                srcSet="https://cdn.sanity.io/images/uozoklvj/production/058eb049746eec9441fbf177179485fc1eb5de12-666x1184.png?w=256&amp;fit=max&amp;auto=format 256w, https://cdn.sanity.io/images/uozoklvj/production/058eb049746eec9441fbf177179485fc1eb5de12-666x1184.png?w=384&amp;fit=max&amp;auto=format 384w, https://cdn.sanity.io/images/uozoklvj/production/058eb049746eec9441fbf177179485fc1eb5de12-666x1184.png?w=640&amp;fit=max&amp;auto=format 640w, https://cdn.sanity.io/images/uozoklvj/production/058eb049746eec9441fbf177179485fc1eb5de12-666x1184.png?w=750&amp;fit=max&amp;auto=format 750w, https://cdn.sanity.io/images/uozoklvj/production/058eb049746eec9441fbf177179485fc1eb5de12-666x1184.png?w=828&amp;fit=max&amp;auto=format 828w, https://cdn.sanity.io/images/uozoklvj/production/058eb049746eec9441fbf177179485fc1eb5de12-666x1184.png?w=1080&amp;fit=max&amp;auto=format 1080w, https://cdn.sanity.io/images/uozoklvj/production/058eb049746eec9441fbf177179485fc1eb5de12-666x1184.png?w=1200&amp;fit=max&amp;auto=format 1200w, https://cdn.sanity.io/images/uozoklvj/production/058eb049746eec9441fbf177179485fc1eb5de12-666x1184.png?w=1920&amp;fit=max&amp;auto=format 1920w, https://cdn.sanity.io/images/uozoklvj/production/058eb049746eec9441fbf177179485fc1eb5de12-666x1184.png?w=2048&amp;fit=max&amp;auto=format 2048w, https://cdn.sanity.io/images/uozoklvj/production/058eb049746eec9441fbf177179485fc1eb5de12-666x1184.png?w=3840&amp;fit=max&amp;auto=format 3840w"
-                src="https://cdn.sanity.io/images/uozoklvj/production/058eb049746eec9441fbf177179485fc1eb5de12-666x1184.png?w=3840&amp;fit=max&amp;auto=format"
-              />
-            </div>
-          </div>
-          <div className="animate-slide-up-in absolute left-[49%] top-0 hidden h-[20%] w-[45%] [animation-delay:0.3s] md:block">
-            <div className="absolute inset-0 h-full w-full overflow-hidden rounded-b-[13px] md:rounded-b-[30px]">
-              <img
-                alt=""
-                fetchPriority="high"
-                decoding="async"
-                data-nimg="fill"
-                className="absolute inset-0 size-full object-cover object-center text-transparent"
-                style={{ objectPosition: "48.6547% 38.9112%" }}
-                sizes="25vw"
-                srcSet="https://cdn.sanity.io/images/uozoklvj/production/908c5efde8bd08e4828dd2d1f97a32498a2aeefe-666x666.png?w=256&amp;fit=max&amp;auto=format 256w, https://cdn.sanity.io/images/uozoklvj/production/908c5efde8bd08e4828dd2d1f97a32498a2aeefe-666x666.png?w=384&amp;fit=max&amp;auto=format 384w, https://cdn.sanity.io/images/uozoklvj/production/908c5efde8bd08e4828dd2d1f97a32498a2aeefe-666x666.png?w=640&amp;fit=max&amp;auto=format 640w, https://cdn.sanity.io/images/uozoklvj/production/908c5efde8bd08e4828dd2d1f97a32498a2aeefe-666x666.png?w=750&amp;fit=max&amp;auto=format 750w, https://cdn.sanity.io/images/uozoklvj/production/908c5efde8bd08e4828dd2d1f97a32498a2aeefe-666x666.png?w=828&amp;fit=max&amp;auto=format 828w, https://cdn.sanity.io/images/uozoklvj/production/908c5efde8bd08e4828dd2d1f97a32498a2aeefe-666x666.png?w=1080&amp;fit=max&amp;auto=format 1080w, https://cdn.sanity.io/images/uozoklvj/production/908c5efde8bd08e4828dd2d1f97a32498a2aeefe-666x666.png?w=1200&amp;fit=max&amp;auto=format 1200w, https://cdn.sanity.io/images/uozoklvj/production/908c5efde8bd08e4828dd2d1f97a32498a2aeefe-666x666.png?w=1920&amp;fit=max&amp;auto=format 1920w, https://cdn.sanity.io/images/uozoklvj/production/908c5efde8bd08e4828dd2d1f97a32498a2aeefe-666x666.png?w=2048&amp;fit=max&amp;auto=format 2048w, https://cdn.sanity.io/images/uozoklvj/production/908c5efde8bd08e4828dd2d1f97a32498a2aeefe-666x666.png?w=3840&amp;fit=max&amp;auto=format 3840w"
-                src="https://cdn.sanity.io/images/uozoklvj/production/908c5efde8bd08e4828dd2d1f97a32498a2aeefe-666x666.png?w=3840&amp;fit=max&amp;auto=format"
-              />
-            </div>
-          </div>
-          <div className="animate-slide-up-in left-initial md:right-initial absolute right-0 top-0 h-[50%] w-[15%] [animation-delay:0.4s] md:left-[98%] md:w-[20%]">
-            <div className="absolute inset-0 h-full w-full overflow-hidden rounded-l-[13px] md:rounded-b-[30px] md:rounded-t-[0]">
-              <img
-                alt=""
-                fetchPriority="high"
-                decoding="async"
-                data-nimg="fill"
-                className="absolute inset-0 size-full object-cover text-transparent"
-                style={{ objectPosition: "48.6547% 38.9112%" }}
-                sizes="25vw"
-                srcSet="https://cdn.sanity.io/images/uozoklvj/production/2ef746a889cf0869fbce78baab31f709083fb31c-333x1184.png?w=256&amp;fit=max&amp;auto=format 256w, https://cdn.sanity.io/images/uozoklvj/production/2ef746a889cf0869fbce78baab31f709083fb31c-333x1184.png?w=384&amp;fit=max&amp;auto=format 384w, https://cdn.sanity.io/images/uozoklvj/production/2ef746a889cf0869fbce78baab31f709083fb31c-333x1184.png?w=640&amp;fit=max&amp;auto=format 640w, https://cdn.sanity.io/images/uozoklvj/production/2ef746a889cf0869fbce78baab31f709083fb31c-333x1184.png?w=750&amp;fit=max&amp;auto=format 750w, https://cdn.sanity.io/images/uozoklvj/production/2ef746a889cf0869fbce78baab31f709083fb31c-333x1184.png?w=828&amp;fit=max&amp;auto=format 828w, https://cdn.sanity.io/images/uozoklvj/production/2ef746a889cf0869fbce78baab31f709083fb31c-333x1184.png?w=1080&amp;fit=max&amp;auto=format 1080w, https://cdn.sanity.io/images/uozoklvj/production/2ef746a889cf0869fbce78baab31f709083fb31c-333x1184.png?w=1200&amp;fit=max&amp;auto=format 1200w, https://cdn.sanity.io/images/uozoklvj/production/2ef746a889cf0869fbce78baab31f709083fb31c-333x1184.png?w=1920&amp;fit=max&amp;auto=format 1920w, https://cdn.sanity.io/images/uozoklvj/production/2ef746a889cf0869fbce78baab31f709083fb31c-333x1184.png?w=2048&amp;fit=max&amp;auto=format 2048w, https://cdn.sanity.io/images/uozoklvj/production/2ef746a889cf0869fbce78baab31f709083fb31c-333x1184.png?w=3840&amp;fit=max&amp;auto=format 3840w"
-                src="https://cdn.sanity.io/images/uozoklvj/production/2ef746a889cf0869fbce78baab31f709083fb31c-333x1184.png?w=3840&amp;fit=max&amp;auto=format"
-              />
-            </div>
-          </div>
-          <div className="animate-slide-up-in absolute bottom-0 left-0 h-[47%] w-[15%] [animation-delay:0.5s] md:w-[45%]">
-            <div className="absolute inset-0 h-full w-full overflow-hidden rounded-r-[13px] md:rounded-[30px]">
-              <img
-                alt=""
-                fetchPriority="high"
-                decoding="async"
-                data-nimg="fill"
-                className="absolute inset-0 size-full object-cover text-transparent"
-                style={{ objectPosition: "48.6547% 38.9112%" }}
-                sizes="25vw"
-                srcSet="https://cdn.sanity.io/images/uozoklvj/production/68894fb65f0c5fd13bc8b4a55b9fe0afb91df941-666x1184.png?w=256&amp;fit=max&amp;auto=format 256w, https://cdn.sanity.io/images/uozoklvj/production/68894fb65f0c5fd13bc8b4a55b9fe0afb91df941-666x1184.png?w=384&amp;fit=max&amp;auto=format 384w, https://cdn.sanity.io/images/uozoklvj/production/68894fb65f0c5fd13bc8b4a55b9fe0afb91df941-666x1184.png?w=640&amp;fit=max&amp;auto=format 640w, https://cdn.sanity.io/images/uozoklvj/production/68894fb65f0c5fd13bc8b4a55b9fe0afb91df941-666x1184.png?w=750&amp;fit=max&amp;auto=format 750w, https://cdn.sanity.io/images/uozoklvj/production/68894fb65f0c5fd13bc8b4a55b9fe0afb91df941-666x1184.png?w=828&amp;fit=max&amp;auto=format 828w, https://cdn.sanity.io/images/uozoklvj/production/68894fb65f0c5fd13bc8b4a55b9fe0afb91df941-666x1184.png?w=1080&amp;fit=max&amp;auto=format 1080w, https://cdn.sanity.io/images/uozoklvj/production/68894fb65f0c5fd13bc8b4a55b9fe0afb91df941-666x1184.png?w=1200&amp;fit=max&amp;auto=format 1200w, https://cdn.sanity.io/images/uozoklvj/production/68894fb65f0c5fd13bc8b4a55b9fe0afb91df941-666x1184.png?w=1920&amp;fit=max&amp;auto=format 1920w, https://cdn.sanity.io/images/uozoklvj/production/68894fb65f0c5fd13bc8b4a55b9fe0afb91df941-666x1184.png?w=2048&amp;fit=max&amp;auto=format 2048w, https://cdn.sanity.io/images/uozoklvj/production/68894fb65f0c5fd13bc8b4a55b9fe0afb91df941-666x1184.png?w=3840&amp;fit=max&amp;auto=format 3840w"
-                src="https://cdn.sanity.io/images/uozoklvj/production/68894fb65f0c5fd13bc8b4a55b9fe0afb91df941-666x1184.png?w=3840&amp;fit=max&amp;auto=format"
-              />
-            </div>
-          </div>
-          <div className="animate-slide-up-in absolute left-[20%] top-0 h-[100%] w-[60%] [animation-delay:0.6s] md:left-[49%] md:top-[23%] md:h-[60%] md:w-[45%]">
-            <div className="absolute inset-0 size-full overflow-hidden rounded-[13px] md:rounded-[30px]">
-              <video
-                playsInline
-                muted
-                loop
-                autoPlay
-                preload="auto"
-                aria-label=""
-                className="absolute inset-0 size-full object-cover"
-              >
-                <source src="https://cdn.sanity.io/files/uozoklvj/production/e593e42c5d117c1c4a170864eb4424eada2ccc8c.mp4" />
-              </video>
-            </div>
-          </div>
-          <div className="animate-slide-up-in absolute bottom-0 left-[49%] hidden h-[14%] w-[45%] [animation-delay:0.7s] md:block">
-            <div className="absolute inset-0 size-full overflow-hidden rounded-[13px] md:rounded-[30px]">
-              <img
-                alt=""
-                fetchPriority="high"
-                decoding="async"
-                data-nimg="fill"
-                className="absolute inset-0 size-full object-cover object-center text-transparent"
-                sizes="25vw"
-                srcSet="https://cdn.sanity.io/images/uozoklvj/production/b9af6d92a3c58a74bb6a372a6192a78a6c3bfe7d-666x333.png?w=256&amp;fit=max&amp;auto=format 256w, https://cdn.sanity.io/images/uozoklvj/production/b9af6d92a3c58a74bb6a372a6192a78a6c3bfe7d-666x333.png?w=384&amp;fit=max&amp;auto=format 384w, https://cdn.sanity.io/images/uozoklvj/production/b9af6d92a3c58a74bb6a372a6192a78a6c3bfe7d-666x333.png?w=640&amp;fit=max&amp;auto=format 640w, https://cdn.sanity.io/images/uozoklvj/production/b9af6d92a3c58a74bb6a372a6192a78a6c3bfe7d-666x333.png?w=750&amp;fit=max&amp;auto=format 750w, https://cdn.sanity.io/images/uozoklvj/production/b9af6d92a3c58a74bb6a372a6192a78a6c3bfe7d-666x333.png?w=828&amp;fit=max&amp;auto=format 828w, https://cdn.sanity.io/images/uozoklvj/production/b9af6d92a3c58a74bb6a372a6192a78a6c3bfe7d-666x333.png?w=1080&amp;fit=max&amp;auto=format 1080w, https://cdn.sanity.io/images/uozoklvj/production/b9af6d92a3c58a74bb6a372a6192a78a6c3bfe7d-666x333.png?w=1200&amp;fit=max&amp;auto=format 1200w, https://cdn.sanity.io/images/uozoklvj/production/b9af6d92a3c58a74bb6a372a6192a78a6c3bfe7d-666x333.png?w=1920&amp;fit=max&amp;auto=format 1920w, https://cdn.sanity.io/images/uozoklvj/production/b9af6d92a3c58a74bb6a372a6192a78a6c3bfe7d-666x333.png?w=2048&amp;fit=max&amp;auto=format 2048w, https://cdn.sanity.io/images/uozoklvj/production/b9af6d92a3c58a74bb6a372a6192a78a6c3bfe7d-666x333.png?w=3840&amp;fit=max&amp;auto=format 3840w"
-                src="https://cdn.sanity.io/images/uozoklvj/production/b9af6d92a3c58a74bb6a372a6192a78a6c3bfe7d-666x333.png?w=3840&amp;fit=max&amp;auto=format"
-              />
-            </div>
-          </div>
-          <div className="animate-slide-up-in left-initial md:right-initial absolute bottom-0 right-0 h-[47%] w-[15%] [animation-delay:0.8s] md:left-[98%] md:w-[20%]">
-            <div className="absolute inset-0 size-full overflow-hidden rounded-l-[13px] md:rounded-[30px]">
-              <img
-                alt=""
-                fetchPriority="high"
-                decoding="async"
-                data-nimg="fill"
-                className="absolute inset-0 size-full object-cover object-center text-transparent"
-                sizes="25vw"
-                srcSet="https://cdn.sanity.io/images/uozoklvj/production/5303fe8347e528c149e95fe23c2fa88cda097dd4-666x1184.png?w=256&amp;fit=max&amp;auto=format 256w, https://cdn.sanity.io/images/uozoklvj/production/5303fe8347e528c149e95fe23c2fa88cda097dd4-666x1184.png?w=384&amp;fit=max&amp;auto=format 384w, https://cdn.sanity.io/images/uozoklvj/production/5303fe8347e528c149e95fe23c2fa88cda097dd4-666x1184.png?w=640&amp;fit=max&amp;auto=format 640w, https://cdn.sanity.io/images/uozoklvj/production/5303fe8347e528c149e95fe23c2fa88cda097dd4-666x1184.png?w=750&amp;fit=max&amp;auto=format 750w, https://cdn.sanity.io/images/uozoklvj/production/5303fe8347e528c149e95fe23c2fa88cda097dd4-666x1184.png?w=828&amp;fit=max&amp;auto=format 828w, https://cdn.sanity.io/images/uozoklvj/production/5303fe8347e528c149e95fe23c2fa88cda097dd4-666x1184.png?w=1080&amp;fit=max&amp;auto=format 1080w, https://cdn.sanity.io/images/uozoklvj/production/5303fe8347e528c149e95fe23c2fa88cda097dd4-666x1184.png?w=1200&amp;fit=max&amp;auto=format 1200w, https://cdn.sanity.io/images/uozoklvj/production/5303fe8347e528c149e95fe23c2fa88cda097dd4-666x1184.png?w=1920&amp;fit=max&amp;auto=format 1920w, https://cdn.sanity.io/images/uozoklvj/production/5303fe8347e528c149e95fe23c2fa88cda097dd4-666x1184.png?w=2048&amp;fit=max&amp;auto=format 2048w, https://cdn.sanity.io/images/uozoklvj/production/5303fe8347e528c149e95fe23c2fa88cda097dd4-666x1184.png?w=3840&amp;fit=max&amp;auto=format 3840w"
-                src="https://cdn.sanity.io/images/uozoklvj/production/5303fe8347e528c149e95fe23c2fa88cda097dd4-666x1184.png?w=3840&amp;fit=max&amp;auto=format"
-              />
-            </div>
-          </div>
         </div>
       </div>
     </div>
